bd7c4955f67bf96665175bfeb2678763f8405324
Georgene-Estevam <78514278+Georgene-Estevam@users.noreply.github.com>
2021-08-10 21:21:14 -0300

    Add files via upload

--- Arquivo JS/main.js
+++ Arquivo JS/main.js
@@ -1,1 +1,9 @@
-$('.col-100').slick();
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autosplaySpeed: 2000
+});
